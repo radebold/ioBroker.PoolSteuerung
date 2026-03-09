621e26363d2f218325b38f7e01cd919db428eef3
Thomas <radebold@gmail.com>
2026-03-09 19:46:31 +0100

    002

--- main.js
+++ main.js
@@ -1,29 +1,48 @@
-
 'use strict';
-
-/*
- Basic starter adapter logic – integrate existing ioBroker scripts
- PH dosing + pump/chlorinator logic placeholders
-*/
 
 const utils = require('@iobroker/adapter-core');
 
 class Poolsteuerung extends utils.Adapter {
-
     constructor(options) {
         super({ ...options, name: 'poolsteuerung' });
         this.on('ready', this.onReady.bind(this));
+        this.on('unload', this.onUnload.bind(this));
+        this.on('stateChange', this.onStateChange.bind(this));
     }
 
     async onReady() {
-        this.log.info('Poolsteuerung adapter started');
+        this.log.info('PoolSteuerung adapter started');
 
-        // TODO integrate logic from scripts
+        await this.setObjectNotExistsAsync('info.connection', {
+            type: 'state',
+            common: {
+                name: 'If adapter is connected',
+                type: 'boolean',
+                role: 'indicator.connected',
+                read: true,
+                write: false,
+                def: false
+            },
+            native: {}
+        });
+
+        await this.setStateAsync('info.connection', true, true);
+    }
+
+    onStateChange(id, state) {
+        if (!state || state.ack) {
+            return;
+        }
+        this.log.debug(`stateChange ${id} = ${JSON.stringify(state.val)}`);
+    }
+
+    onUnload(callback) {
+        this.setState('info.connection', false, true, () => callback());
     }
 }
 
 if (require.main !== module) {
-    module.exports = (options) => new Poolsteuerung(options);
+    module.exports = options => new Poolsteuerung(options);
 } else {
     new Poolsteuerung();
 }
