e055b1c675b288e88eb508291cd5166cc422044a
Thomas <radebold@gmail.com>
2026-03-19 22:53:48 +0100

    0395

--- build/main.js
+++ build/main.js
@@ -200,6 +200,14 @@ class Poolsteuerung extends utils.Adapter {
     await this.setStateAsync('info.poolVolume', volume, true);
   }
 
+
+  wrapHtmlForVis(htmlContent) {
+    const escaped = String(htmlContent)
+      .replace(/&/g, '&amp;')
+      .replace(/"/g, '&quot;');
+    return `<iframe style="width:100%;height:100%;border:0;display:block;border-radius:18px;background:transparent;" srcdoc="${escaped}"></iframe>`;
+  }
+
   async renderVis() {
     const ph = this.fmt(await this.getNumber(this.config.phStateId, 2), 2);
     const orp = this.fmt(await this.getNumber(this.config.orpStateId, 0), 0);
@@ -229,6 +237,12 @@ class Poolsteuerung extends utils.Adapter {
     await this.ensureState('vis.htmlPhone', 'string', 'html', '', false);
     await this.setStateAsync('vis.htmlTablet', tablet, true);
     await this.setStateAsync('vis.htmlPhone', phone, true);
+    await this.ensureState('vis.urlTablet', 'string', 'text.url', '', false);
+    await this.ensureState('vis.urlPhone', 'string', 'text.url', '', false);
+    await this.writeFileAsync('vis.0', 'user/pool/tablet.html', tablet);
+    await this.writeFileAsync('vis.0', 'user/pool/phone.html', phone);
+    await this.setStateAsync('vis.urlTablet', '/vis.0/user/pool/tablet.html', true);
+    await this.setStateAsync('vis.urlPhone', '/vis.0/user/pool/phone.html', true);
     await this.ensureState('status.debug.lastVisUpdate', 'string', 'text', '', false);
     await this.setStateAsync('status.debug.lastVisUpdate', data.updated, true);
   }
@@ -310,10 +324,3 @@ if (require.main !== module) {
 } else {
   (() => new Poolsteuerung())();
 }
-
-    await this.ensureState('vis.urlTablet', 'string', 'text.url', '', false);
-    await this.ensureState('vis.urlPhone', 'string', 'text.url', '', false);
-    await this.writeFileAsync('vis.0', 'user/pool/tablet.html', tablet);
-    await this.writeFileAsync('vis.0', 'user/pool/phone.html', phone);
-    await this.setStateAsync('vis.urlTablet', '/vis.0/user/pool/tablet.html', true);
-    await this.setStateAsync('vis.urlPhone', '/vis.0/user/pool/phone.html', true);
